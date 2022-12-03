f1d5013ebe23004d0df5dd0b9364013784ed52df
Will Killian <william.killian@outlook.com>
2022-12-03 01:29:11 -0500

    Day03: update implementation
    Signed-off-by: Will Killian <william.killian@outlook.com>

--- days/Day03.cpp
+++ days/Day03.cpp
@@ -1,10 +1,7 @@
 #include <algorithm>
 
 #include <cctype>
-#include <fmt/ranges.h>
-#include <string_view>
 
-#include "Day.hpp"
 #include "Day03.hpp"
 
 PARSE_IMPL(Day03, buffer) {
@@ -19,44 +16,42 @@ PARSE_IMPL(Day03, buffer) {
   return result;
 }
 
+inline int
+score(char c) noexcept {
+  return (c - (::isupper(c) ? ('A' - 26) : 'a')) + 1;
+}
 
-PART1_IMPL(Day03, parse_result) {
+PART1_IMPL(Day03, lines) {
   int answer{0};
-  for (auto const &line : parse_result) {
-    std::array<char, 64> L, R, T;
-    auto const lhs = line.substr(0, line.size() / 2);
-    auto const rhs = line.substr(line.size() / 2);
-    auto end_l = std::copy(std::begin(lhs), std::end(lhs), std::begin(L));
-    std::sort(std::begin(L), end_l);
-    auto end_r = std::copy(std::begin(rhs), std::end(rhs), std::begin(R));
-    std::sort(std::begin(R), end_r);
-    std::set_intersection(std::begin(L), end_l, std::begin(R), end_r, std::begin(T));
-    if (char const out{*std::begin(T)}; ::isupper(out)) {
-      answer += 26 + (out - 'A') + 1;
-    } else {
-      answer += (out - 'a') + 1;
-    }
+  for (auto const &line : lines) {
+    std::array<char, 64> A, B, C;
+    sz const mid{line.size() / 2};
+    std::string_view const lhs = line.substr(0, mid);
+    std::string_view const rhs = line.substr(mid);
+    auto const A1 = std::copy(std::begin(lhs), std::end(lhs), std::begin(A));
+    std::sort(std::begin(A), A1);
+    auto const B1 = std::copy(std::begin(rhs), std::end(rhs), std::begin(B));
+    std::sort(std::begin(B), B1);
+    std::set_intersection(std::begin(A), A1, std::begin(B), B1, std::begin(C));
+    answer += score(C.front());
   }
   return answer;
 }
 
-PART2_IMPL(Day03, parse_result, part1_answer) {
+PART2_IMPL(Day03, lines, part1_answer) {
   int answer{0};
-  for (sz idx{0}; idx < std::size(parse_result); idx += 3) {
-    std::array<char, 64> L, R, T;
-    auto end_l = std::copy(std::begin(parse_result[idx]), std::end(parse_result[idx]), std::begin(L));
-    std::sort(std::begin(L), end_l);
-    auto end_r = std::copy(std::begin(parse_result[idx + 1]), std::end(parse_result[idx + 1]), std::begin(R));
-    std::sort(std::begin(R), end_r);
-    auto const end_t = std::set_intersection(std::begin(L), end_l, std::begin(R), end_r, std::begin(T));
-    end_l = std::copy(std::begin(parse_result[idx + 2]), std::end(parse_result[idx + 2]), std::begin(L));
-    std::sort(std::begin(L), end_l);
-    std::set_intersection(std::begin(L), end_l, std::begin(T), end_t, std::begin(R));
-    if (char const out{*std::begin(R)}; ::isupper(out)) {
-      answer += 26 + (out - 'A') + 1;
-    } else {
-      answer += (out - 'a') + 1;
-    }
+  for (sz idx{0}; idx < std::size(lines); idx += 3) {
+    std::array<char, 64> A, B, C;
+    std::array<char, 32> T, U;
+    auto const A1 = std::copy(std::begin(lines[idx + 0]), std::end(lines[idx + 0]), std::begin(A));
+    std::sort(std::begin(A), A1);
+    auto const B1 = std::copy(std::begin(lines[idx + 1]), std::end(lines[idx + 1]), std::begin(B));
+    std::sort(std::begin(B), B1);
+    auto const C1 = std::copy(std::begin(lines[idx + 2]), std::end(lines[idx + 2]), std::begin(C));
+    std::sort(std::begin(C), C1);
+    auto const T1 = std::set_intersection(std::begin(A), A1, std::begin(B), B1, std::begin(T));
+    std::set_intersection(std::begin(C), C1, std::begin(T), T1, std::begin(U));
+    answer += score(U.front());
   }
   return answer;
 }
