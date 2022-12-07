3ba675fbc65bb6509378bbe76420ca692f36610e
Will Killian <william.killian@outlook.com>
2022-12-07 18:59:08 -0500

    Day 7: update solution

--- days/day07.cpp
+++ days/day07.cpp
@@ -1,118 +1,55 @@
+#include <iterator>
+#include <numeric>
+
 #include <doctest/doctest.h>
 
 #include "days/day07.hpp"
 #include "parsing.hpp"
 
 PARSE_IMPL(Day07, view) {
-  using namespace day07;
-  // always create root
-  auto root = std::make_unique<dir>("/"sv, nullptr);
-  auto curr = root.get();
 
-  for (u32 off{7}; off < std::size(view);) {
-    if (view.substr(off + 2, 2) == "ls"sv) {
-      // command is '$ ls'
-      // advance to next line
-      off += 5;
-      // while we have something in our directory
-      while (off < std::size(view) and view[off] != '$') {
-        if (view[off] == 'd') {
-          // we have a directory
-          std::string_view new_dir;
-          off += parse<"dir \0\n">(view.substr(off), new_dir);
-          curr->dirs.push_back(new dir{new_dir, curr});
-        } else {
-          // we have a file
-          u32 size;
-          off += parse<"\0 ">(view.substr(off), size);
-          off += view.substr(off).find_first_of('\n') + 1;
-          // update directory sizes
-          for (auto *d = curr; d != nullptr; d = d->parent) {
-            d->size += size;
-          }
-        }
+  std::vector<u32> sizes;
+  std::vector<u32> dir_stack;
+
+  for (u32 off{0}; off < std::size(view);) {
+    if ('0' <= view[off] and view[off] <= '9') {
+      u32 size;
+      off += parse<"\0 ">(view.substr(off), size);
+      for (auto &dir : dir_stack) {
+        dir += size;
       }
-    } else {
-      // command is '$ cd <dir>'
-      // advance to <dir>
-      off += 5;
-      std::string_view dir_name;
-      off += parse<"\0\n">(view.substr(off), dir_name);
-      // special case of '..'
-      if (dir_name == ".."sv) {
-        curr = curr->parent;
+    } else if (view.substr(off + 2, 2) == "cd"sv) {
+      off += 5; // advance to directory name
+      if (view[off] == '.') {
+        sizes.push_back(dir_stack.back());
+        dir_stack.pop_back();
       } else {
-        // search all children, looking for matching dir
-        for (auto *node : curr->dirs) {
-          if (node->name == dir_name) {
-            curr = node;
-            break;
-          }
-        }
+        dir_stack.push_back(0u);
       }
     }
+    off += view.substr(off).find_first_of('\n') + 1;
   }
-  return root;
+  sizes.reserve(std::size(sizes) + std::size(dir_stack));
+  std::copy(std::rbegin(dir_stack), std::rend(dir_stack), std::back_insert_iterator(sizes));
+  return sizes;
 }
 
-class filter_visitor {
-  u32 filtered{0};
-
-public:
-  inline void visit(day07::dir *dir) noexcept {
-    for (day07::dir *d : dir->dirs) {
-      visit(d);
-    }
-    if (dir->size < 100'000) {
-      filtered += dir->size;
-    }
-  }
-
-  inline u32 answer() const noexcept {
-    return filtered;
-  }
-};
-
-class find_min_visitor {
-  u32 lower;
-  u32 the_smallest{-1u};
-
-public:
-  find_min_visitor() = delete;
-
-  explicit find_min_visitor(u32 target)
-      : lower{target} {
-  }
-
-  inline void visit(day07::dir *dir) noexcept {
-    for (day07::dir *d : dir->dirs) {
-      visit(d);
-    }
-    if (dir->size >= lower && dir->size < the_smallest) {
-      the_smallest = dir->size;
-    }
-  }
-
-  inline u32 answer() const noexcept {
-    return the_smallest;
-  }
-};
-
-PART1_IMPL(Day07, root) {
-  filter_visitor v;
-  v.visit(root.get());
-  return v.answer();
+PART1_IMPL(Day07, sizes) {
+  return std::accumulate(std::begin(sizes), std::end(sizes), 0u, [](u32 acc, u32 s) {
+    return acc + (s < 100'000 ? s : 0u);
+  });
 }
 
-PART2_IMPL(Day07, root, part1_answer) {
-  u32 const used_space{root->size};
-  constexpr u32 const total_space{70'000'000};
-  constexpr u32 const needed_free_space{30'000'000};
+constexpr u32 const total_space{70'000'000};
+constexpr u32 const needed_free_space{30'000'000};
+
+PART2_IMPL(Day07, sizes, part1_answer) {
+  u32 const used_space{sizes.back()};
   u32 const available_space{total_space - used_space};
   u32 const need_to_free{needed_free_space - available_space};
-  find_min_visitor m{need_to_free};
-  m.visit(root.get());
-  return m.answer();
+  return std::accumulate(std::begin(sizes), std::end(sizes), -1u, [need_to_free](u32 best_fit, u32 s) {
+    return ((s >= need_to_free && s < best_fit) ? s : best_fit);
+  });
 }
 
 INSTANTIATE_TEST(Day07,
