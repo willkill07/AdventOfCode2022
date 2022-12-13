028a3438c57e78711f00bdb8ab762c3591414c13
Will Killian <william.killian@nextsilicon.com>
2022-12-12 22:57:30 -0500

    General: remove code so AppleClang is happy
    Signed-off-by: Will Killian <william.killian@nextsilicon.com>

--- days/include/point2d.hpp
+++ days/include/point2d.hpp
@@ -62,10 +62,6 @@ struct point2d {
 
   friend constexpr inline auto operator<=>(point2d const &, point2d const &) = default;
 
-  constexpr usize hash() const noexcept {
-    return std::bit_cast<usize>(*this);
-  }
-
   static constexpr inline point2d right() noexcept {
     return {1, 0};
   }
@@ -84,11 +80,5 @@ struct point2d {
 
   static constexpr inline point2d origin() noexcept {
     return {0, 0};
-  }
-};
-
-struct point2d_hash {
-  constexpr inline usize operator()(point2d const &p) const noexcept {
-    return p.hash();
   }
 };
