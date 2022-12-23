73915393875098e30bbca34df1c77708db96ff14
Will Killian <william.killian@outlook.com>
2022-12-23 10:25:59 -0500

    Day 22: add intermediate work
    Signed-off-by: Will Killian <william.killian@outlook.com>

--- days/day22.cpp
+++ days/day22.cpp
@@ -18,78 +18,81 @@ gcd(u32 a, u32 b) noexcept {
   return a;
 }
 
-inline std::tuple<u32, u32, u32, direction>
-step(std::array<side, 6> &sides, u32 const side_size, u32 side, u32 x, u32 y, direction dir) noexcept {
+constexpr inline direction
+adjust(direction curr, direction shift) noexcept {
+  u32 const num{std::to_underlying(direction::num_dirs)};
+  u32 const shift_amount{shift == direction::right ? 1u : 3u};
+  u32 const index{(std::to_underlying(curr) + shift_amount) % num};
+  return direction{index};
+}
+
+inline auto
+step(owning_span<side, 6> const &sides,
+     u32 const side_size,
+     u32 const side,
+     u32 const x,
+     u32 const y,
+     direction const dir) noexcept {
+
+  u32 const max_index{side_size - 1};
 
   // staying within the same side
   if (dir == direction::left and x > 0) {
-    return {side, x - 1, y, dir};
-  } else if (dir == direction::right and x + 1 < side_size) {
-    return {side, x + 1, y, dir};
+    bool const res{sides[side].valid(x - 1, y)};
+    return std::tuple(res, side, x - res, y, dir);
+  } else if (dir == direction::right and x < max_index) {
+    bool const res{sides[side].valid(x + 1, y)};
+    return std::tuple(res, side, x + res, y, dir);
   } else if (dir == direction::up and y > 0) {
-    return {side, x, y - 1, dir};
-  } else if (dir == direction::down and y + 1 < side_size) {
-    return {side, x, y + 1, dir};
+    bool const res{sides[side].valid(x, y - 1)};
+    return std::tuple(res, side, x, y - res, dir);
+  } else if (dir == direction::down and y < max_index) {
+    bool const res{sides[side].valid(x, y + 1)};
+    return std::tuple(res, side, x, y + res, dir);
   }
+
+  u32 const side_offset{[](u32 max_idx, direction d, u32 x, u32 y) noexcept {
+    switch (d) {
+    case direction::right:
+      return y;
+    case direction::down:
+      return max_idx - x;
+    case direction::left:
+      return max_idx - y;
+    case direction::up:
+      return x;
+    default:
+      __builtin_unreachable();
+    }
+  }(max_index, dir, x, y)};
+
+  direction const new_dir{sides[side].neighbors[std::to_underlying(dir)].dir};
+
+  auto const [new_x, new_y] = [](u32 max_idx, direction d, u32 offset) noexcept {
+    switch (d) {
+    case direction::right:
+      return std::tuple(0u, offset);
+    case direction::down:
+      return std::tuple(max_idx - offset, 0u);
+    case direction::left:
+      return std::tuple(0u, max_idx - offset);
+    case direction::up:
+      return std::tuple(offset, 0u);
+    default:
+      __builtin_unreachable();
+    }
+  }(max_index, new_dir, side_offset);
 
   u32 const new_side{sides[side].neighbors[std::to_underlying(dir)].id};
 
-  // if we have no side, return same location
-  if (new_side == 0xFFFFFFFF) {
-    return {side, x, y, dir};
+  if (sides[new_side].valid(new_x, new_y)) {
+    return std::tuple(true, new_side, new_x, new_y, new_dir);
+  } else {
+    return std::tuple(false, side, x, y, dir);
   }
-
-  u32 const side_offset{(dir == direction::left or dir == direction::right) ? y : x};
-
-  direction const incoming{sides[side].neighbors[std::to_underlying(dir)].dir};
-
-  direction const new_dir{[] (direction d) {
-    switch (d) {
-      case direction::right: return direction::left;
-      case direction::down: return direction::up;
-      case direction::left: return direction::right;
-      case direction::up: return direction::down;
-      default: __builtin_unreachable();
-    }
-  }(incoming)};
-
-  switch (dir) {
-    case direction::right:
-      switch (new_dir) {
-        case direction::right: return {new_side, 0, side_offset, new_dir};
-        case direction::down: return {new_side, side_offset, 0, new_dir};
-        case direction::left: return {new_side, 0, (side_size - 1) - side_offset, new_dir};
-        case direction::up: return {new_side, (side_size - 1) - side_offset, 0, new_dir};
-        default: __builtin_unreachable();
-      }
-    case direction::down:
-      switch (new_dir) {
-        case direction::right: return {new_side, 0, (side_size - 1) - side_offset, new_dir};
-        case direction::down: return {new_side, side_offset, 0, new_dir};
-        case direction::left: return {new_side, 0, side_offset, new_dir};
-        case direction::up: return {new_side, (side_size - 1) - side_offset, 0, new_dir};
-        default: __builtin_unreachable();
-      }
-    case direction::left:
-      switch (new_dir) {
-        case direction::right: return {new_side, 0, (side_size - 1) - side_offset, new_dir};
-        case direction::down: return {new_side, (side_size - 1) - side_offset, 0, new_dir};
-        case direction::left: return {new_side, 0, side_offset, new_dir};
-        case direction::up: return {new_side, side_offset, 0, new_dir};
-        default: __builtin_unreachable();
-      }
-    case direction::up:
-      switch (new_dir) {
-        case direction::right: return {new_side, 0, side_offset, new_dir};
-        case direction::down: return {new_side, (side_size - 1) - side_offset, 0, new_dir};
-        case direction::left: return {new_side, 0, (side_size - 1) - side_offset, new_dir};
-        case direction::up: return {new_side, side_offset, 0, new_dir};
-        default: __builtin_unreachable();
-      }
-    default: __builtin_unreachable();
-  }
-  
 }
+
+#include <fmt/core.h>
 
 PARSE_IMPL(Day22, view) {
   auto const [height, width, side_len] = [&] {
@@ -106,7 +109,7 @@ PARSE_IMPL(Day22, view) {
 
   owning_span<side, 6> sides;
   u32 skip{1}, global_row{0}, len{0};
-  u32 off{0}; 
+  u32 off{0};
   for (; view[off] != '\n'; off += len + 1, ++global_row) {
     len = as<u32>(view.find_first_of('\n', off)) - off;
     if (--skip != 0) {
@@ -129,8 +132,8 @@ PARSE_IMPL(Day22, view) {
         // when we've inserted at least two faces in the same row
         if (global_col - sides[new_id - 1].global_col == side_len) {
           // and they are exactly side_len away, create the relationships
-          sides[new_id].neighbors[std::to_underlying(direction::left)] = {new_id - 1, direction::right};
-          sides[new_id - 1].neighbors[std::to_underlying(direction::right)] = {new_id, direction::left};
+          sides[new_id].neighbors[std::to_underlying(direction::left)] = {new_id - 1, direction::left};
+          sides[new_id - 1].neighbors[std::to_underlying(direction::right)] = {new_id, direction::right};
         }
       }
 
@@ -143,8 +146,8 @@ PARSE_IMPL(Day22, view) {
         } else if (diff == side_len) {
           if (global_col == sides[id].global_col) {
             // when vertically adjacent and columns align, we are connected
-            sides[id].neighbors[std::to_underlying(direction::down)] = {new_id, direction::up};
-            sides[new_id].neighbors[std::to_underlying(direction::up)] = {id, direction::down};
+            sides[id].neighbors[std::to_underlying(direction::down)] = {new_id, direction::down};
+            sides[new_id].neighbors[std::to_underlying(direction::up)] = {id, direction::up};
           }
         }
       }
@@ -152,22 +155,129 @@ PARSE_IMPL(Day22, view) {
   }
 
   ++off;
-  owning_span<command, 2'000> cmds;
-  while (view[off] != '\n') {
-    // TODO: parse command by reading a single character, then constructing a number
-    // finally, push into owning span
+  owning_span<command, 2'048> cmds;
+  u32 dist{as<u32>(view[off++] - '0')};
+  while ('0' <= view[off] and view[off] <= '9') {
+    dist = dist * 10 + as<u32>(view[off++] - '0');
+  }
+  cmds.push({direction::right, dist});
+  while (off + 1 < std::size(view)) {
+    // read single character for direction
+    direction const dir{view[off++] == 'R' ? direction::right : direction::left};
+    // read number
+    dist = as<u32>(view[off++] - '0');
+    while ('0' <= view[off] and view[off] <= '9') {
+      dist = dist * 10 + as<u32>(view[off++] - '0');
+    }
+    cmds.push({dir, dist});
   }
 
-  return {sides, cmds};
+  return {sides, cmds, side_len};
 }
 
-PART1_IMPL(Day22, data) {
-  return {};
+SOLVE_IMPL(Day22, Part2, data, part1_answer) {
+  auto sides{data.sides};
+  if constexpr (not Part2) {
+    if (data.side_len != 50) {
+      //   0
+      // 123
+      //   45
+      sides[0].neighbors[std::to_underlying(direction::right)] = {0, direction::right};
+      sides[0].neighbors[std::to_underlying(direction::left)] = {0, direction::left};
+      sides[0].neighbors[std::to_underlying(direction::up)] = {4, direction::up};
+      sides[1].neighbors[std::to_underlying(direction::down)] = {1, direction::down};
+      sides[1].neighbors[std::to_underlying(direction::left)] = {3, direction::left};
+      sides[1].neighbors[std::to_underlying(direction::up)] = {1, direction::up};
+      sides[2].neighbors[std::to_underlying(direction::down)] = {2, direction::down};
+      sides[2].neighbors[std::to_underlying(direction::up)] = {2, direction::up};
+      sides[3].neighbors[std::to_underlying(direction::right)] = {1, direction::right};
+      sides[4].neighbors[std::to_underlying(direction::left)] = {5, direction::left};
+      sides[4].neighbors[std::to_underlying(direction::down)] = {0, direction::down};
+      sides[5].neighbors[std::to_underlying(direction::right)] = {4, direction::right};
+      sides[5].neighbors[std::to_underlying(direction::up)] = {5, direction::up};
+      sides[5].neighbors[std::to_underlying(direction::down)] = {5, direction::down};
+    } else {
+      //  01
+      //  2
+      // 34
+      // 5
+      sides[0].neighbors[std::to_underlying(direction::left)] = {1, direction::left};
+      sides[0].neighbors[std::to_underlying(direction::up)] = {4, direction::up};
+      sides[1].neighbors[std::to_underlying(direction::right)] = {0, direction::right};
+      sides[1].neighbors[std::to_underlying(direction::down)] = {1, direction::down};
+      sides[1].neighbors[std::to_underlying(direction::up)] = {1, direction::up};
+      sides[2].neighbors[std::to_underlying(direction::right)] = {2, direction::right};
+      sides[2].neighbors[std::to_underlying(direction::left)] = {2, direction::left};
+      sides[3].neighbors[std::to_underlying(direction::left)] = {4, direction::left};
+      sides[3].neighbors[std::to_underlying(direction::up)] = {5, direction::up};
+      sides[4].neighbors[std::to_underlying(direction::right)] = {3, direction::right};
+      sides[4].neighbors[std::to_underlying(direction::down)] = {0, direction::down};
+      sides[5].neighbors[std::to_underlying(direction::right)] = {5, direction::right};
+      sides[5].neighbors[std::to_underlying(direction::down)] = {3, direction::down};
+      sides[5].neighbors[std::to_underlying(direction::left)] = {5, direction::left};
+    }
+  } else {
+    if (data.side_len != 50) {
+      //   0
+      // 123
+      //   45
+      sides[0].neighbors[std::to_underlying(direction::right)] = {5, direction::left};
+      sides[0].neighbors[std::to_underlying(direction::up)] = {1, direction::down};
+      sides[0].neighbors[std::to_underlying(direction::left)] = {2, direction::down};
+      sides[1].neighbors[std::to_underlying(direction::left)] = {5, direction::up};
+      sides[1].neighbors[std::to_underlying(direction::up)] = {0, direction::down};
+      sides[1].neighbors[std::to_underlying(direction::down)] = {4, direction::up};
+      sides[2].neighbors[std::to_underlying(direction::up)] = {0, direction::right};
+      sides[2].neighbors[std::to_underlying(direction::down)] = {4, direction::left};
+      sides[3].neighbors[std::to_underlying(direction::right)] = {5, direction::down};
+      sides[4].neighbors[std::to_underlying(direction::down)] = {1, direction::right};
+      sides[4].neighbors[std::to_underlying(direction::left)] = {1, direction::up};
+      sides[5].neighbors[std::to_underlying(direction::right)] = {0, direction::left};
+      sides[5].neighbors[std::to_underlying(direction::down)] = {1, direction::right};
+      sides[5].neighbors[std::to_underlying(direction::up)] = {3, direction::left};
+    } else {
+      //  01
+      //  2
+      // 34
+      // 5
+      sides[0].neighbors[std::to_underlying(direction::left)] = {3, direction::right};
+      sides[0].neighbors[std::to_underlying(direction::up)] = {5, direction::right};
+      sides[1].neighbors[std::to_underlying(direction::right)] = {4, direction::left};
+      sides[1].neighbors[std::to_underlying(direction::down)] = {2, direction::left};
+      sides[1].neighbors[std::to_underlying(direction::up)] = {5, direction::up};
+      sides[2].neighbors[std::to_underlying(direction::left)] = {3, direction::down};
+      sides[2].neighbors[std::to_underlying(direction::right)] = {1, direction::up};
+      sides[3].neighbors[std::to_underlying(direction::left)] = {0, direction::right};
+      sides[3].neighbors[std::to_underlying(direction::up)] = {2, direction::right};
+      sides[4].neighbors[std::to_underlying(direction::right)] = {1, direction::left};
+      sides[4].neighbors[std::to_underlying(direction::down)] = {5, direction::left};
+      sides[5].neighbors[std::to_underlying(direction::left)] = {0, direction::down};
+      sides[5].neighbors[std::to_underlying(direction::right)] = {4, direction::up};
+      sides[5].neighbors[std::to_underlying(direction::down)] = {1, direction::down};
+    }
+  }
+
+  u32 const size{data.side_len};
+
+  direction dir{direction::up};
+  u32 side{0}, x{0}, y{0};
+
+  for (auto const [rotate, amount] : data.commands) {
+    dir = adjust(dir, rotate);
+
+    for (u32 i{0}; i < amount; ++i) {
+      bool moved{false};
+      std::tie(moved, side, x, y, dir) = step(sides, size, side, x, y, dir);
+      if (not moved) {
+        break;
+      }
+    }
+  }
+
+  return sides[side].get_uid(x, y, dir);
 }
 
-PART2_IMPL(Day22, data, part1_answer) {
-  return {};
-}
+INSTANTIATE(Day22);
 
 INSTANTIATE_TEST(Day22,
                  R"(
@@ -186,5 +296,5 @@ INSTANTIATE_TEST(Day22,
 
 10R5L5R10L4R5L5
 )"sv.substr(1),
-                 24000,
-                 45000)
+                 6032,
+                 5031)
--- days/include/days/day22.hpp
+++ days/include/days/day22.hpp
@@ -1,4 +1,3 @@
-#include <array>
 #include <utility>
 
 #include "days/day.hpp"
@@ -6,7 +5,7 @@
 
 namespace day22 {
 
-enum class direction : u32 { right = 0, down = 1, left = 2, up = 3, none = 0xFFFFFFFF };
+enum class direction : u32 { right = 0, down = 1, left = 2, up = 3, num_dirs = 4, none = 0xFFFFFFFF };
 
 struct neighbor {
   u32 id{0xFFFFFFFF};
@@ -20,21 +19,26 @@ struct side {
   u32 global_row{0};
   u32 global_col{0};
 
-  [[nodiscard]] inline constexpr bool connected(direction d) const noexcept {
-    return neighbors[std::to_underlying(d)].id != 0xFFFFFFFF;
+  [[nodiscard]] constexpr inline bool valid(u32 x, u32 y) const noexcept {
+    return buffer[y * stride + x] != '#';
+  }
+
+  [[nodiscard]] constexpr inline u32 get_uid(u32 x, u32 y, direction d) const noexcept {
+    return std::to_underlying(d) + 4 * (global_col + x + 1) + 1000 * (global_row + y + 1);
   }
 };
 
 struct command {
-    direction rotate;
-    u32 distance;
+  direction rotate;
+  u32 distance;
 };
 
 struct result {
-    owning_span<side, 6> sides;
-    owning_span<command, 2'000U> commands;
+  owning_span<side, 6> sides;
+  owning_span<command, 2'048U> commands;
+  u32 side_len;
 };
 
-}
+} // namespace day22
 
 using Day22 = Day<22, day22::result, u32>;
